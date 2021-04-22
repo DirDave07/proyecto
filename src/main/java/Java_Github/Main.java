0a09df42d1feb696eef98cca4e8b00641bd3fab0
DirDave07 <dofh07@gmail.com>
2021-04-22 15:01:58 -0600

    hola buenas

--- src/main/java/Java_Github/Main.java
+++ src/main/java/Java_Github/Main.java
@@ -16,6 +16,14 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int resultado = sumar(5,10);
+        System.out.print(resultado);
     }
-    
+    public static int sumar(int inicio, int fin){
+        if(fin>inicio){
+            return fin + sumar (inicio, fin-1);
+        }else{
+            return fin;
+        }
+    }
 }
